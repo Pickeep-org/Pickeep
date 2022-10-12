4851883708a4927d253fc6fd8cf30b5f103e8fca
orel0 <orel0@lala.com>
2022-10-12 21:17:43 +0300

    locations from json and districts

--- lib/filters.dart
+++ lib/filters.dart
@@ -1,11 +1,35 @@
 import 'package:pickeep/firestore/firestore_categories.dart';
 import 'package:pickeep/firestore/firestore_locatoins.dart';
 
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 // TODO: refactor
+
+
+
 class Filters {
+
+  Future locationsList() async {
+    var url = 'https://firebasestorage.googleapis.com/v0/b/pickeep-3341c.appspot.com/o/files%2Fdatastore_cities.json?alt=media&token=8ffbf7e5-0e98-4d02-84ca-a131db95c62e';
+    http.Response response = await http.get(Uri.parse(url));
+    var user = jsonDecode(response.body);
+    var answer = user['result']['records'];
+    districts =  answer.expand((e)=>[e[1]]).toSet().toList().cast<String>()..sort();
+    locations = answer.expand((e)=>[e[0]]).toList().cast<String>();
+    for(int i = 0; i < districts.length; i++) {
+      var result = answer.where((e)=> e[1] == districts[i]);
+      districtsLocations[ districts[i] ] = result.expand((e)=>[e[0]]).toList();
+    }
+    return;
+  }
+
   static final Filters _instance = Filters._intInstance();
   late List<String> locations;
+  late List<String> districts;
   late List<String> categories;
+  late Map<String, List<dynamic>> districtsLocations = {};
+  //late List<Map<String, String>> districts;
 
   factory Filters() {
     return _instance;
@@ -14,10 +38,8 @@ class Filters {
   Filters._intInstance();
 
   Future loadFilters() async {
-    locations = (await FirestoreLocations.instance().getLocationsOrderByCity())
-        .docs
-        .map((doc) => doc.get('city').toString())
-        .toList();
+    await locationsList();
+    print(districts);
     categories = (await FirestoreCategories.instance().getCategoriesOrderByName())
         .docs
         .map((doc) => doc.get('name').toString())
